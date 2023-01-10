233ac7c8ee2db50b838a8bccff86e371c734effc
vhyen <vanhoangyen0206@gmail.com>
2023-01-10 18:22:13 +0700

    create order

--- repynn/order/views.py
+++ repynn/order/views.py
@@ -1,5 +1,32 @@
-from django.urls import reverse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from .models import OrderItem
+from .forms import OrderCreateForm
+from cart.cart import Cart
+
 # Create your views here.
 
-
+@login_required
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                         customer=request.user,
+                                         dish=item['dish'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+            # clear the cart
+            cart.clear()
+            return render(request, 'orders/order/created.html',
+                          {'order': order})
+        else:
+            form = OrderCreateForm()
+        return render(request, 'orders/order/create.html', {
+            'cart': cart,
+            'form': form
+        })
--- repynn/repynn/urls.py
+++ repynn/repynn/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('dish/', include('dish.urls', namespace='restaurant')),
     path('payment/', include('payment.urls', namespace='payment'))
 ]
